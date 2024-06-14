321b28836914fcfdae883489ef3ab1c79258ea59
Sebastian <61376632+nrgsille76@users.noreply.github.com>
2024-06-14 14:29:03 +0200

    CleanUp: Import_max

--- io_scene_max/import_max.py
+++ io_scene_max/import_max.py
@@ -1,3 +1,575 @@
+# SPDX-FileCopyrightText: 2023-2024 Sebastian Schrand
+#                         2017-2022 Jens M. Plonka
+#                         2005-2018 Philippe Lagadec
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Import is based on using information from `olefile` IO source-code
+# and the FreeCAD Autodesk 3DS Max importer ImportMAX.
+#
+# `olefile` (formerly OleFileIO_PL) is copyright Philippe Lagadec.
+# (https://www.decalage.info)
+#
+# ImportMAX is copyright Jens M. Plonka.
+# (https://www.github.com/jmplonka/Importer3D)
+
+import io
+import os
+import re
+import sys
+import bpy
+import math
+import zlib
+import array
+import struct
+import mathutils
+from pathlib import Path
+from bpy_extras.image_utils import load_image
+from bpy_extras.node_shader_utils import PrincipledBSDFWrapper
+
+
+###################
+# DATA STRUCTURES #
+###################
+
+MAGIC = b'\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1'
+WORD_CLSID = "00020900-0000-0000-C000-000000000046"
+
+MIN_FILE_SIZE = 1536
+UNKNOWN_SIZE = 0x7FFFFFFF
+MAXFILE_SIZE = 0x7FFFFFFFFFFFFFFF
+MAXREGSECT = 0xFFFFFFFA  # (-6) maximum SECT
+DIFSECT = 0xFFFFFFFC  # (-4) denotes a DIFAT sector in a FAT
+FATSECT = 0xFFFFFFFD  # (-3) denotes a FAT sector in a FAT
+ENDOFCHAIN = 0xFFFFFFFE  # (-2) end of a virtual stream chain
+FREESECT = 0xFFFFFFFF  # (-1) unallocated sector
+MAX_STREAM = 2  # element is a stream object
+ROOT_STORE = 5  # element is a root storage
+
+TYP_NAME = 0x0962
+INVALID_NAME = re.compile('^[0-9].*')
+UNPACK_BOX_DATA = struct.Struct('<HIHHBff').unpack_from  # Index, int, 2short, byte, 2float
+
+FLOAT_POINT = 0x71F11549498702E7  # Float Wire
+MATRIX_POS = 0xFFEE238A118F7E02  # Position XYZ
+MATRIX_ROT = 0x3A90416731381913  # Rotation Wire
+MATRIX_SCL = 0xFEEE238B118F7C01  # Scale XYZ
+BIPED_OBJ = 0x0000000000009125  # Biped Object
+BIPED_ANIM = 0x78C6B2A6B147369  # Biped SubAnim
+EDIT_MESH = 0x00000000E44F10B3  # Editable Mesh
+EDIT_POLY = 0x192F60981BF8338D  # Editable Poly
+CORO_MTL = 0x448931dd70be6506  # CoronaMtl
+ARCH_MTL = 0x4A16365470B05735  # ArchMtl
+VRAY_MTL = 0x7034695C37BF3F2F  # VRayMtl
+DUMMY = 0x0000000000876234  # Dummy
+
+SKIPPABLE = {
+    0x0000000000001002: 'Camera',
+    0x0000000000001011: 'Omni',
+    0x0000000000001013: 'Free Direct',
+    0x0000000000001020: 'Camera Target',
+    0x0000000000001040: 'Line',
+    0x0000000000001065: 'Rectangle',
+    0x0000000000001097: 'Ellipse',
+    0x0000000000001999: 'Circle',
+    0x0000000000002013: 'Point',
+    0x05622B0D69011E82: 'Compass',
+    0x12A822FB76A11646: 'CV Surface',
+    0x1EB3430074F93B07: 'Particle View',
+    0x2ECCA84028BF6E8D: 'Bone',
+    0x3BDB0E0C628140F6: 'VRayPlane',
+    0x4E9B599047DB14EF: 'Slider',
+    0x522E47057BF61478: 'Sky',
+    0x5FD602DF3C5575A1: 'VRayLight',
+    0x77566F65081F1DFC: 'Plane',
+}
+
+CONFIG = []
+CLS_DATA = []
+DLL_DIR_LIST = []
+CLS_DIR3_LIST = []
+VID_PST_QUE = []
+SCENE_LIST = []
+
+object_list = []
+object_dict = {}
+parent_dict = {}
+matrix_dict = {}
+materials = set()
+
+
+def get_valid_name(name):
+    if (INVALID_NAME.match(name)):
+        return "_%s" % (name.encode('utf8'))
+    return "%s" % (name.encode('utf8'))
+
+
+def i8(data):
+    return data if data.__class__ is int else data[0]
+
+
+def i16(data, offset=0):
+    return struct.unpack("<H", data[offset:offset + 2])[0]
+
+
+def i32(data, offset=0):
+    return struct.unpack("<I", data[offset:offset + 4])[0]
+
+
+def get_byte(data, offset=0):
+    size = offset + 1
+    value = struct.unpack('<B', data[offset:size])[0]
+    return value, size
+
+
+def get_short(data, offset=0):
+    size = offset + 2
+    value = struct.unpack('<H', data[offset:size])[0]
+    return value, size
+
+
+def get_long(data, offset=0):
+    size = offset + 4
+    value = struct.unpack('<I', data[offset:size])[0]
+    return value, size
+
+
+def get_float(data, offset=0):
+    size = offset + 4
+    value = struct.unpack('<f', data[offset:size])[0]
+    return value, size
+
+
+def get_bytes(data, offset=0, count=1):
+    size = offset + count
+    values = struct.unpack('<' + 'B' * count, data[offset:size])
+    return values, size
+
+
+def get_shorts(data, offset=0, count=1):
+    size = offset + count * 2
+    values = struct.unpack('<' + 'H' * count, data[offset:size])
+    return values, size
+
+
+def get_longs(data, offset=0, count=1):
+    size = offset + count * 4
+    values = struct.unpack('<' + 'I' * count, data[offset:size])
+    return values, size
+
+
+def get_floats(data, offset=0, count=1):
+    size = offset + count * 4
+    values = struct.unpack('<' + 'f' * count, data[offset:size])
+    return values, size
+
+
+def _clsid(clsid):
+    """Converts a CLSID to a readable string."""
+    assert len(clsid) == 16
+    if not clsid.strip(b"\0"):
+        return ""
+    return (("%08X-%04X-%04X-%02X%02X-" + "%02X" * 6) %
+            ((i32(clsid, 0), i16(clsid, 4), i16(clsid, 6)) +
+            tuple(map(i8, clsid[8:16]))))
+
+
+###############
+# DATA IMPORT #
+###############
+
+def is_maxfile(filename):
+    """Test if file is a MAX OLE2 container."""
+    if hasattr(filename, 'read'):
+        header = filename.read(len(MAGIC))
+        filename.seek(0)
+    elif isinstance(filename, bytes) and len(filename) >= MIN_FILE_SIZE:
+        header = filename[:len(MAGIC)]
+    else:
+        with open(filename, 'rb') as fp:
+            header = fp.read(len(MAGIC))
+    if header == MAGIC:
+        return True
+    else:
+        return False
+
+
+class MaxStream(io.BytesIO):
+    """Returns an instance of the BytesIO class as read-only file object."""
+
+    def __init__(self, fp, sect, size, offset, sectorsize, fat, filesize):
+        if size == UNKNOWN_SIZE:
+            size = len(fat) * sectorsize
+        nb_sectors = (size + (sectorsize - 1)) // sectorsize
+
+        data = []
+        for i in range(nb_sectors):
+            try:
+                fp.seek(offset + sectorsize * sect)
+            except:
+                break
+            sector_data = fp.read(sectorsize)
+            data.append(sector_data)
+            try:
+                sect = fat[sect] & FREESECT
+            except IndexError:
+                break
+        data = b"".join(data)
+        if len(data) >= size:
+            data = data[:size]
+            self.size = size
+        else:
+            self.size = len(data)
+        io.BytesIO.__init__(self, data)
+
+
+class MaxFileDirEntry:
+    """Directory Entry for a stream or storage."""
+    STRUCT_DIRENTRY = '<64sHBBIII16sIQQIII'
+    DIRENTRY_SIZE = 128
+    assert struct.calcsize(STRUCT_DIRENTRY) == DIRENTRY_SIZE
+
+    def __init__(self, entry, sid, maxfile):
+        self.sid = sid
+        self.maxfile = maxfile
+        self.kids = []
+        self.kids_dict = {}
+        self.used = False
+        (
+            self.name_raw,
+            self.namelength,
+            self.entry_type,
+            self.color,
+            self.sid_left,
+            self.sid_right,
+            self.sid_child,
+            clsid,
+            self.dwUserFlags,
+            self.createTime,
+            self.modifyTime,
+            self.isectStart,
+            self.sizeLow,
+            self.sizeHigh
+        ) = struct.unpack(MaxFileDirEntry.STRUCT_DIRENTRY, entry)
+
+        if self.namelength > 64:
+            self.namelength = 64
+        self.name_utf16 = self.name_raw[:(self.namelength - 2)]
+        self.name = maxfile._decode_utf16_str(self.name_utf16)
+        # print('DirEntry SID=%d: %s' % (self.sid, repr(self.name)))
+        if maxfile.sectorsize == 512:
+            self.size = self.sizeLow
+        else:
+            self.size = self.sizeLow + (int(self.sizeHigh) << 32)
+        self.clsid = _clsid(clsid)
+        self.is_minifat = False
+        if self.entry_type in (ROOT_STORE, MAX_STREAM) and self.size > 0:
+            if self.size < maxfile.minisectorcutoff \
+                    and self.entry_type == MAX_STREAM:  # only streams can be in MiniFAT
+                self.is_minifat = True
+            else:
+                self.is_minifat = False
+            maxfile._check_duplicate_stream(self.isectStart, self.is_minifat)
+        self.sect_chain = None
+
+    def build_sect_chain(self, maxfile):
+        if self.sect_chain:
+            return
+        if self.entry_type not in (ROOT_STORE, MAX_STREAM) or self.size == 0:
+            return
+        self.sect_chain = list()
+        if self.is_minifat and not maxfile.minifat:
+            maxfile.loadminifat()
+        next_sect = self.isectStart
+        while next_sect != ENDOFCHAIN:
+            self.sect_chain.append(next_sect)
+            if self.is_minifat:
+                next_sect = maxfile.minifat[next_sect]
+            else:
+                next_sect = maxfile.fat[next_sect]
+
+    def build_storage_tree(self):
+        if self.sid_child != FREESECT:
+            self.append_kids(self.sid_child)
+            self.kids.sort()
+
+    def append_kids(self, child_sid):
+        if child_sid == FREESECT:
+            return
+        else:
+            child = self.maxfile._load_direntry(child_sid)
+            if child.used:
+                return
+            child.used = True
+            self.append_kids(child.sid_left)
+            name_lower = child.name.lower()
+            self.kids.append(child)
+            self.kids_dict[name_lower] = child
+            self.append_kids(child.sid_right)
+            child.build_storage_tree()
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __le__(self, other):
+        return self.__eq__(other) or self.__lt__(other)
+
+
+class ImportMaxFile:
+    """Representing an interface for importing .max files."""
+
+    def __init__(self, filename=None):
+        self._filesize = None
+        self.byte_order = None
+        self.directory_fp = None
+        self.direntries = None
+        self.dll_version = None
+        self.fat = None
+        self.first_difat_sector = None
+        self.first_dir_sector = None
+        self.first_mini_fat_sector = None
+        self.fp = None
+        self.header_clsid = None
+        self.header_signature = None
+        self.mini_sector_shift = None
+        self.mini_sector_size = None
+        self.mini_stream_cutoff_size = None
+        self.minifat = None
+        self.minifatsect = None
+        self.minisectorcutoff = None
+        self.minisectorsize = None
+        self.ministream = None
+        self.minor_version = None
+        self.nb_sect = None
+        self.num_difat_sectors = None
+        self.num_dir_sectors = None
+        self.num_fat_sectors = None
+        self.num_mini_fat_sectors = None
+        self.reserved1 = None
+        self.reserved2 = None
+        self.root = None
+        self.sector_shift = None
+        self.sector_size = None
+        self.transaction_signature_number = None
+        if filename:
+            self.open(filename)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+    def _decode_utf16_str(self, utf16_str, errors='replace'):
+        unicode_str = utf16_str.decode('UTF-16LE', errors)
+        return unicode_str
+
+    def open(self, filename):
+        if hasattr(filename, 'read'):
+            self.fp = filename
+        elif isinstance(filename, bytes) and len(filename) >= MIN_FILE_SIZE:
+            self.fp = io.BytesIO(filename)
+        else:
+            self.fp = open(filename, 'rb')
+        filesize = 0
+        self.fp.seek(0, os.SEEK_END)
+        try:
+            filesize = self.fp.tell()
+        finally:
+            self.fp.seek(0)
+        self._filesize = filesize
+        self._used_streams_fat = []
+        self._used_streams_minifat = []
+        header = self.fp.read(512)
+        fmt_header = '<8s16sHHHHHHLLLLLLLLLL'
+        header_size = struct.calcsize(fmt_header)
+        header1 = header[:header_size]
+        (
+            self.header_signature,
+            self.header_clsid,
+            self.minor_version,
+            self.dll_version,
+            self.byte_order,
+            self.sector_shift,
+            self.mini_sector_shift,
+            self.reserved1,
+            self.reserved2,
+            self.num_dir_sectors,
+            self.num_fat_sectors,
+            self.first_dir_sector,
+            self.transaction_signature_number,
+            self.mini_stream_cutoff_size,
+            self.first_mini_fat_sector,
+            self.num_mini_fat_sectors,
+            self.first_difat_sector,
+            self.num_difat_sectors
+        ) = struct.unpack(fmt_header, header1)
+
+        self.sector_size = 2**self.sector_shift
+        self.mini_sector_size = 2**self.mini_sector_shift
+        if self.mini_stream_cutoff_size != 0x1000:
+            self.mini_stream_cutoff_size = 0x1000
+        self.nb_sect = ((filesize + self.sector_size - 1) // self.sector_size) - 1
+
+        # file clsid
+        self.header_clsid = _clsid(header[8:24])
+        self.sectorsize = self.sector_size  # i16(header, 30)
+        self.minisectorsize = self.mini_sector_size   # i16(header, 32)
+        self.minisectorcutoff = self.mini_stream_cutoff_size  # i32(header, 56)
+        self._check_duplicate_stream(self.first_dir_sector)
+        if self.num_mini_fat_sectors:
+            self._check_duplicate_stream(self.first_mini_fat_sector)
+        if self.num_difat_sectors:
+            self._check_duplicate_stream(self.first_difat_sector)
+
+        # Load file allocation tables
+        self.loadfat(header)
+        self.loaddirectory(self.first_dir_sector)
+        self.minifatsect = self.first_mini_fat_sector
+
+    def close(self):
+        self.fp.close()
+
+    def _check_duplicate_stream(self, first_sect, minifat=False):
+        if minifat:
+            used_streams = self._used_streams_minifat
+        else:
+            if first_sect in (DIFSECT, FATSECT, ENDOFCHAIN, FREESECT):
+                return
+            used_streams = self._used_streams_fat
+        if first_sect in used_streams:
+            pass
+        else:
+            used_streams.append(first_sect)
+
+    def sector_array(self, sect):
+        ary = array.array('I', sect)
+        if sys.byteorder == 'big':
+            ary.byteswap()
+        return ary
+
+    def loadfat_sect(self, sect):
+        if isinstance(sect, array.array):
+            fat1 = sect
+        else:
+            fat1 = self.sector_array(sect)
+        isect = None
+        for isect in fat1:
+            isect = isect & FREESECT
+            if isect == ENDOFCHAIN or isect == FREESECT:
+                break
+            sector = self.getsect(isect)
+            nextfat = self.sector_array(sector)
+            self.fat = self.fat + nextfat
+        return isect
+
+    def loadfat(self, header):
+        sect = header[76:512]
+        self.fat = array.array('I')
+        self.loadfat_sect(sect)
+        if self.num_difat_sectors != 0:
+            nb_difat_sectors = (self.sectorsize // 4) - 1
+            nb_difat = (self.num_fat_sectors - 109 + nb_difat_sectors - 1) // nb_difat_sectors
+            isect_difat = self.first_difat_sector
+            for i in range(nb_difat):
+                sector_difat = self.getsect(isect_difat)
+                difat = self.sector_array(sector_difat)
+                self.loadfat_sect(difat[:nb_difat_sectors])
+                isect_difat = difat[nb_difat_sectors]
+        if len(self.fat) > self.nb_sect:
+            self.fat = self.fat[:self.nb_sect]
+
+    def loadminifat(self):
+        stream_size = self.num_mini_fat_sectors * self.sector_size
+        nb_minisectors = (self.root.size + self.mini_sector_size - 1) // self.mini_sector_size
+        used_size = nb_minisectors * 4
+        sect = self._open(self.minifatsect, stream_size, force_FAT=True).read()
+        self.minifat = self.sector_array(sect)
+        self.minifat = self.minifat[:nb_minisectors]
+
+    def getsect(self, sect):
+        try:
+            self.fp.seek(self.sectorsize * (sect + 1))
+        except:
+            print('IndexError: Sector index out of range')
+        sector = self.fp.read(self.sectorsize)
+        return sector
+
+    def loaddirectory(self, sect):
+        self.directory_fp = self._open(sect, force_FAT=True)
+        max_entries = self.directory_fp.size // 128
+        self.direntries = [None] * max_entries
+        root_entry = self._load_direntry(0)
+        self.root = self.direntries[0]
+        self.root.build_storage_tree()
+
+    def _load_direntry(self, sid):
+        if self.direntries[sid] is not None:
+            return self.direntries[sid]
+        self.directory_fp.seek(sid * 128)
+        entry = self.directory_fp.read(128)
+        self.direntries[sid] = MaxFileDirEntry(entry, sid, self)
+        return self.direntries[sid]
+
+    def _open(self, start, size=UNKNOWN_SIZE, force_FAT=False):
+        if size < self.minisectorcutoff and not force_FAT:
+            if not self.ministream:
+                self.loadminifat()
+                size_ministream = self.root.size
+                self.ministream = self._open(self.root.isectStart,
+                                             size_ministream, force_FAT=True)
+            return MaxStream(fp=self.ministream, sect=start, size=size,
+                             offset=0, sectorsize=self.minisectorsize,
+                             fat=self.minifat, filesize=self.ministream.size)
+        else:
+            return MaxStream(fp=self.fp, sect=start, size=size,
+                             offset=self.sectorsize, sectorsize=self.sectorsize,
+                             fat=self.fat, filesize=self._filesize)
+
+    def _find(self, filename):
+        if isinstance(filename, str):
+            filename = filename.split('/')
+        node = self.root
+        for name in filename:
+            for kid in node.kids:
+                if kid.name.lower() == name.lower():
+                    break
+            node = kid
+        return node.sid
+
+    def openstream(self, filename):
+        sid = self._find(filename)
+        entry = self.direntries[sid]
+        return self._open(entry.isectStart, entry.size)
+
+
+###################
+# DATA PROCESSING #
+###################
+
+class MaxChunk():
+    """Representing a chunk of a .max file."""
+
+    def __init__(self, types, size, level, number):
+        self.number = number
+        self.types = types
+        self.level = level
+        self.parent = None
+        self.previous = None
+        self.following = None
+        self.size = size
+        self.data = None
+
+    def __str__(self):
+        return "%s[%4x]%04X:%s" % ("" * self.level, self.number, self.types, self.data)
+
+
 class ByteArrayChunk(MaxChunk):
     """A byte array of a .max chunk."""
 
