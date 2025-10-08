6bf30093ad6716604792ae6da364374de9705db6
NRG Sille <61376632+nrgsille76@users.noreply.github.com>
2025-10-08 12:33:32 +0200

    io_scene_3ds: Bump version

--- io_scene_3ds/io_scene_3ds/__init__.py
+++ io_scene_3ds/io_scene_3ds/__init__.py
@@ -5,7 +5,7 @@
 
 
 __author__ = "Sebastian Sille <nrgsille@gmail.com>"
-__version__ = "2.8.5"
+__version__ = "2.8.6"
 __date__ = "24 Sep 2020"
 
 
@@ -41,7 +41,7 @@ if "bpy" in locals():
 bl_info = {
     "name": "Autodesk 3D Studio (.3ds)",
     "author": "Bob Holcomb, Campbell Barton, Sebastian Schrand",
-    "version": (2, 8, 5),
+    "version": (2, 8, 6),
     "blender": (4, 0, 0),
     "location": "File > Import-Export",
     "description": "3DS Import/Export meshes, UVs, materials, textures, "
